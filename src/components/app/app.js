7aaa8df7bc7ab469fb080a1966758a0f97589386
Thai Tran <Thai56@Thais-MacBook-Air.local>
2017-02-10 20:52:58 -0700

    adding parallax

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,27 +3,30 @@ import IMG_URL from '../../img/BarbershopLogo.jpg'
 import Header from '../header/header'
 import Footer from '../footer/footer'
 import './reset.scss'
+import { Parallax , Background} from 'react-parallax';
+
 export default class App extends Component {
   render(){
-    const backgroundStyle = {
-      height:'100vh',
-      width:'100vw',
-      background:`#B09B7E`,
-      backgroundSize:'cover',
-      backgroundPosition:'center'
-    }
-    const sectionStyle ={
-      background:`url(${IMG_URL})no-repeat`,
-      backgroundSize:'100% 100%',
-      backgroundPosition:'center'
-    }
+  //   const backgroundStyle = {
+  //     height:'100vh',
+  //     width:'100vw',
+  //     background:`#B09B7E`,
+  //     backgroundSize:'cover',
+  //     backgroundPosition:'center'
+  //   }
+
     return (
-      <section style={backgroundStyle}>
+      <section id='app-container'>
+
+      <Parallax bgImage='src/img/BarberShopLogo.jpg' strength={70}>
+
       <Header />
-      <div id="body">
+        <div id="body">
         {this.props.children}
       </div>
       <Footer />
+      </Parallax>
+
       </section>
     )
   }
--- src/components/home/home.js
+++ src/components/home/home.js
@@ -1,5 +1,5 @@
 import React , { Component } from 'react'
-
+import './home.scss'
 export default class Home extends Component {
   render(){
     return <section id='home'>
