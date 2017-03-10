90a9823448f4fb4cab8b4811a7a3da6f529ffeef
Thai Tran <Thai56@Thais-MacBook-Air.local>
2017-03-10 10:49:35 -0700

    text aligned

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -11,21 +11,22 @@ export default function About() {
 
     <header>
       <div className="about-title">
-        About the Barbershop
+        The Barbershop
       </div>
     </header>
 
     <div id="about-pic" />
 
       <section className='summary-wrapper'>
-      The Revolucion Tattoo and Barber Shop in Provo is a place of community and
-       acceptance. Our IMMEDIATE goal is to make you look amazing… whether you see E’Sau,
+      The Revolución Tattoo and Barber Shop in Provo is a place of community and
+       acceptance. Our IMMEDIATE goal is to make you look amazing… whether you see E’Sau or
        Chipi. The ULTIMATE goal is to make friends, build a happy world and
        have a community full of differences.
 
 Our clientele is forever growing because we’re cool people,
-making cool people look great. Whatever your hair type – we know how to make
-you look incredible. You’ll leave feeling like gold.
+making cool people look great. Whatever your hair type,
+ we know how to make you look incredible.
+You’ll leave feeling like gold.
       </section>
       <div className="place-holder">
         Second Location Coming Soon!!
